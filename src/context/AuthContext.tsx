3b9c2b6b205f6de645c6538dea8c3aba2d5ab241
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2025-11-05 11:07:28 +0100

    Add files via upload

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -482,3 +482,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
+
+export { AuthContext }
+export default AuthProvider
